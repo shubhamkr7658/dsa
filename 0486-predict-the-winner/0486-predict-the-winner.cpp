e44efd769de7a71a39a2b95f0aab1d4cf066c0e9
shubhamkr7658 <shubhamkr76588@gmail.com>
2026-08-01 15:54:34 +0530

    Time: 127 ms (8.96%), Space: 9.7 MB (69.71%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.cpp
+++ 0486-predict-the-winner/0486-predict-the-winner.cpp
@@ -1,53 +1,21 @@
 class Solution {
-
 public:
-
-int dp[21][21][3];
-
-int sol(vector<int>& nums,int l,int r,int f){
-
-    if(l>r)return 0;
-
-    if(dp[l][r][f]!=-1)return dp[l][r][f];
-
+int solve(vector<int>&nums,int i,int j,int f){
+    if(i>j){
+        return 0;
+    }
+    int left,right=0;
     if(f){
-
-        return min(sol(nums,l+1,r,0),sol(nums,l,r-1,0));
-
+     return min(solve(nums,i+1,j,0),solve(nums,i,j-1,0));
     }
-
-    int rht = 0,lft=0;
-
-    
-
-    lft = nums[l]+sol(nums,l+1,r,1);
-
-    rht=nums[r]+sol(nums,l,r-1,1);
-
-
-
-    return dp[l][r][f] = max(lft,rht);
-
-    
-
+    return max((nums[i]+solve(nums,i+1,j,1)),nums[j]+solve(nums,i,j-1,1));
 }
-
-bool predictTheWinner(vector<int>& nums) {
-
-    int sum = 0;
-
-    for(auto&el:nums){
-
-        sum+=el;
-
+    bool predictTheWinner(vector<int>& nums) {
+        int ans=solve(nums,0,nums.size()-1,0);
+        int sum=0;
+        for(auto &t:nums){
+       sum+=t;
+        }
+        return (sum-ans<=ans);
     }
-
-    memset(dp,-1,sizeof(dp));
-
-    int ans = sol(nums,0,nums.size()-1,0);
-
-    return sum-ans<=ans;
-
-}
-
 };
